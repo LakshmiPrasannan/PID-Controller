2c1f6b0b3c9a5c4071a91dad7ad34db5bc554015
LakshmiPrasannan <jicksy.john@gmail.com>
2020-06-13 21:00:11 +0530

    First Commit

---
+++ CarND-PID-Control-Project/README.md
@@ -0,0 +1,39 @@
+# CarND-Controls-PID
+Self-Driving Car Engineer Nanodegree Program
+
+A link to the car making it around the track is posted below.
+https://youtu.be/qfA8V237iEw
+
+The parameters for the controller were chosen according to trial and error method. As suggested in the lesson by Sebastian I tried impletting a twiddle implementation. However, the twiddle was very slow to train. Twiddle cost function was looking for minimum error, which meant that the car was unstable when at the middle of the road and just swayed. Though it was smoother at the sides, it did veer off at sides. 
+ The parameters also were greatly affected by the speed of the car. When higher speed like a 40MPH there was a lot of disturbance in the route followed. Note the speed portion was also governed by a pid controller. The steer PID controller used the P,I,D parameters, -.1, -0.0005, -.5. The I term turned out to be pretty insignificant even though the simulated car had a baised steering angle. The baised steering angle meant that if the car drove with zero steer turning then it would veer off to the right. The speed PID had P,I,D parameters of 0.3, 0.002, 0.0, so it was just a PI controller.
+
+The P term greatly impacted the amount of sway that the car had, the larger the value the larger distance it would osscillate. A high P value was important for the car to do tight turns, but too large and it would sway when going stright. The D term was very important for dampening the sway factor, and was set just by experimentation. The I term although not having a large impact to the overall system was being used to help offset the steering biasis.
+
+Although the car could go up to 30 MPH with the parameters chosen, it was set back down to 20 MPH just to be safe.
+
+---
+
+## Dependencies
+
+* cmake >= 3.5
+ * All OSes: [click here for installation instructions](https://cmake.org/install/)
+* make >= 4.1
+  * Linux: make is installed by default on most Linux distros
+  * Mac: [install Xcode command line tools to get make](https://developer.apple.com/xcode/features/)
+  * Windows: [Click here for installation instructions](http://gnuwin32.sourceforge.net/packages/make.htm)
+* gcc/g++ >= 5.4
+  * Linux: gcc / g++ is installed by default on most Linux distros
+  * Mac: same deal as make - [install Xcode command line tools]((https://developer.apple.com/xcode/features/)
+  * Windows: recommend using [MinGW](http://www.mingw.org/)
+* [uWebSockets](https://github.com/uWebSockets/uWebSockets) == 0.13, but the master branch will probably work just fine
+  * Follow the instructions in the [uWebSockets README](https://github.com/uWebSockets/uWebSockets/blob/master/README.md) to get setup for your platform. You can download the zip of the appropriate version from the [releases page](https://github.com/uWebSockets/uWebSockets/releases). Here's a link to the [v0.13 zip](https://github.com/uWebSockets/uWebSockets/archive/v0.13.0.zip).
+  * If you run OSX and have homebrew installed you can just run the ./install-mac.sh script to install this
+* Simulator. You can download these from the [project intro page](https://classroom.udacity.com/nanodegrees/nd013/parts/40f38239-66b6-46ec-ae68-03afd8a601c8/modules/aca605f8-8219-465d-9c5d-ca72c699561d/lessons/e8235395-22dd-4b87-88e0-d108c5e5bbf4/concepts/6a4d8d42-6a04-4aa6-b284-1697c0fd6562) in the classroom.
+
+## Basic Build Instructions
+
+1. Clone this repo.
+2. Make a build directory: `mkdir build && cd build`
+3. Compile: `cmake .. && make`
+4. Run it: `./pid`.
+
---
+++ CarND-PID-Control-Project/src/main.cpp
@@ -0,0 +1,247 @@
+#include <uWS/uWS.h>
+#include <iostream>
+#include "json.hpp"
+#include "PID.h"
+#include <math.h>
+
+#include "Twiddle.h"
+
+using namespace std;
+
+// for convenience
+using json = nlohmann::json;
+
+// For converting back and forth between radians and degrees.
+constexpr double pi() { return M_PI; }
+double deg2rad(double x) { return x * pi() / 180; }
+double rad2deg(double x) { return x * 180 / pi(); }
+
+// Checks if the SocketIO event has JSON data.
+// If there is data the JSON object in string format will be returned,
+// else the empty string "" will be returned.
+std::string hasData(std::string s) {
+  auto found_null = s.find("null");
+  auto b1 = s.find_first_of("[");
+  auto b2 = s.find_last_of("]");
+  if (found_null != std::string::npos) {
+    return "";
+  }
+  else if (b1 != std::string::npos && b2 != std::string::npos) {
+    return s.substr(b1, b2 - b1 + 1);
+  }
+  return "";
+}
+
+int main()
+{
+  uWS::Hub h;
+
+  PID pid;
+
+  PID speedPID;
+
+  // 30MPH -0.1, -0.00005, -0.5
+
+  pid.Init(-0.1,-0.0005,-0.5);
+  speedPID.Init(0.3,0.002,0.0);
+
+  PrintResults();
+
+  h.onMessage([&pid,&speedPID](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
+    // "42" at the start of the message means there's a websocket message event.
+    // The 4 signifies a websocket message
+    // The 2 signifies a websocket event
+
+    if (length && length > 2 && data[0] == '4' && data[1] == '2')
+    {
+      auto s = hasData(std::string(data));
+      if (s != "") {
+        auto j = json::parse(s);
+        std::string event = j[0].get<std::string>();
+        if (event == "telemetry") {
+          // j[1] is the data JSON object
+          double cte = std::stod(j[1]["cte"].get<std::string>());
+          double speed = std::stod(j[1]["speed"].get<std::string>());
+          //double angle = std::stod(j[1]["steering_angle"].get<std::string>());
+          double steer_value;
+      	  double speed_value;
+  	  double setSpeed = 20.0;
+
+  	  		
+          /*
+          * TODO: Calcuate steering value here, remember the steering value is
+          * [-1, 1].
+          * NOTE: Feel free to play around with the throttle and speed. Maybe use
+          * another PID controller to control the speed!
+          */
+
+
+          steer_value = pid.Kp*cte+pid.Ki*pid.i_error+pid.Kd*pid.d_error;
+          speed_value = speedPID.Kp*(setSpeed-speed)+speedPID.Ki*speedPID.i_error+speedPID.Kd*speedPID.d_error;
+	  	  pid.UpdateError(cte);  
+	  	  speedPID.UpdateError((setSpeed-speed));
+	
+          // DEBUG
+          //std::cout << "Cycle: " << pid.cycle_n << " Error "<< pid.TotalError() << std::endl;
+
+	  bool twiddle = false;
+	  
+	  if(twiddle && (pid.TotalError() > GetBestError() ||  pid.cycle_n > 4000 || ((speed < setSpeed*.2) && (pid.cycle_n > 50 ))  ) )
+	  {
+	     if((speed < setSpeed*.2)&&(pid.cycle_n > 50))
+	     {
+		cout << "Car got stuck!" << endl;
+		pid= UpdateTwiddle(pid, 1000000);
+	     }
+	     else
+	     {
+	     	pid = UpdateTwiddle(pid, pid.TotalError());
+	     }
+	     PrintResults();
+             std::string msg = "42[\"reset\",{}]";
+	     ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+          }
+	  else
+	  {
+             json msgJson;
+             msgJson["steering_angle"] = steer_value;
+             msgJson["throttle"] = speed_value;
+             auto msg = "42[\"steer\"," + msgJson.dump() + "]";
+             // std::cout << msg << std::endl;
+             ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+	  } 
+
+
+        }
+      } else {
+        // Manual driving
+        std::string msg = "42[\"manual\",{}]";
+        ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+      }
+    }
+
+  });
+
+  // We don't need this since we're not using HTTP but if it's removed the program
+  // doesn't compile :-(
+  h.onHttpRequest([](uWS::HttpResponse *res, uWS::HttpRequest req, char *data, size_t, size_t) {
+    const std::string s = "<h1>Hello world!</h1>";
+    if (req.getUrl().valueLength == 1)
+    {
+      res->end(s.data(), s.length());
+    }
+    else
+    {
+      // i guess this should be done more gracefully?
+      res->end(nullptr, 0);
+    }
+  });
+
+  h.onConnection([&h](uWS::WebSocket<uWS::SERVER> ws, uWS::HttpRequest req) {
+    std::cout << "Connected!!!" << std::endl;
+  });
+
+  h.onDisconnection([&h](uWS::WebSocket<uWS::SERVER> ws, int code, char *message, size_t length) {
+    ws.close();
+    std::cout << "Disconnected" << std::endl;
+  });
+
+  int port = 4567;
+  if (h.listen(port))
+  {
+    std::cout << "Listening to port " << port << std::endl;
+  }
+  else
+  {
+    std::cerr << "Failed to listen to port" << std::endl;
+    return -1;
+  }
+  h.run();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
